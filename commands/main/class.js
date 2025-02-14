52a91221197d9749894163aaa4ac3ace69060f71
StrangeAlmond <strangealmond@strangealmond.com>
2025-02-14 16:09:22 -0500

    fix bug

--- commands/main/class.js
+++ commands/main/class.js
@@ -103,7 +103,7 @@ export default {
         this.joinClass(interaction, classID);
 
         const responseEmbed = this.responseEmbed(interaction);
-        responseEmbed.setDescription(`Successfully joined the \`${interaction.guildData.options[classID.toLowerCase()].name}\` class.`);
+        responseEmbed.setDescription(`Successfully joined the \`${interaction.guildData.classes[classID.toLowerCase()].name}\` class.`);
 
         return await interaction.reply({ embeds: [responseEmbed] });
     },
